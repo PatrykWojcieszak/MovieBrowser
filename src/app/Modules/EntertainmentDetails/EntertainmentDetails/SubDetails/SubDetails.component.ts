c7bbfc731923bcf507e1d36bb1c7d7b6386885c9
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-10 18:27:15 +0100

    feat: passed data to subDetails component

--- src/app/Modules/EntertainmentDetails/EntertainmentDetails/SubDetails/SubDetails.component.ts
+++ src/app/Modules/EntertainmentDetails/EntertainmentDetails/SubDetails/SubDetails.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-SubDetails',
   templateUrl: './SubDetails.component.html',
-  styleUrls: ['./SubDetails.component.scss']
+  styleUrls: ['./SubDetails.component.scss'],
 })
 export class SubDetailsComponent implements OnInit {
+  @Input() budget: number = 0;
+  @Input() revenue: number = 0;
+  @Input() status: string = '';
+  @Input() language: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
