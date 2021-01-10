f77fd84beb78d31a7aba316c29509a62e10146a7
Patryk Wojcieszak <patryk.1996.19@o2.pl>
2021-01-10 15:03:19 +0100

    feat: pass fetched data to components

--- src/app/Modules/EntertainmentDetails/EntertainmentDetails/Header/Header.component.ts
+++ src/app/Modules/EntertainmentDetails/EntertainmentDetails/Header/Header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import IGenre from 'src/app/Models/IGenre';
 @Component({
   selector: 'app-Header',
   templateUrl: './Header.component.html',
@@ -7,6 +8,13 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 })
 export class HeaderComponent implements OnInit {
   faArrowLeft = faArrowLeft;
+
+  @Input() title: string = '';
+  @Input() date: string = '';
+  @Input() runtime: number = 0;
+  @Input() avgVote: number = 0;
+  @Input() genres: IGenre[] = [];
+
   constructor() {}
 
   ngOnInit() {}
--- src/app/Modules/EntertainmentDetails/EntertainmentDetails/MainDetails/MainDetails.component.ts
+++ src/app/Modules/EntertainmentDetails/EntertainmentDetails/MainDetails/MainDetails.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-MainDetails',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./MainDetails.component.scss'],
 })
 export class MainDetailsComponent implements OnInit {
+  @Input() img: string = '';
+  @Input() overview: string = '';
+
   constructor() {}
 
   ngOnInit() {}
